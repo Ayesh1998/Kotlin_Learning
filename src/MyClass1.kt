b8e5bb8e66f9482a6c180f2959223fa14a2bae1c
Ayesh1998 <ayeshlak1998@gmail.com>
2019-09-03 19:42:01 +0530

    Classes,Functions,Constructor,Variables finished

--- src/MyClass1.kt
+++ src/MyClass1.kt
@@ -1,3 +1,54 @@
+import com.exmple.Car
+
 fun main(args: Array<String>) {
-    print("Hi kotlin")
+
+//    Variables
+
+    var no = 10
+    var name = "Ayesh"
+//    define and declare after that
+    var NameMy: String
+    NameMy = "Lakshan"
+
+//    final in kotlin
+    val fixedName = "Ayesh Lakshan"
+
+//    println("Hi kotlin")
+//    show("Ayesh")
+
+    var person = Person()
+    person.showDetails("Ayesh KOlla")
+    person.names = "Ayeshs"
+    person.show()
+
+//    concatanation without plus sign
+    println("Name with concatnation ${person.names}")
+
+    var car = Car("Benz")
+    car.display()
+
 }
+
+//    creating function
+fun show(name:String){
+    println(name)
+}
+
+
+
+//Creting a class and method inside it
+class Person{
+
+    var names:String = ""
+    fun showDetails(name:String){
+        println(name)
+    }
+
+    fun show(){
+        println(names)
+    }
+
+}
+
+
+
---
+++ src/com/exmple/Car.kt
@@ -0,0 +1,9 @@
+package com.exmple
+//class with a constructor
+
+class Car(var name:String) {
+
+    fun  display (){
+        println("Name of the car is $name")
+    }
+}
